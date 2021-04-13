781d9481ae3080648b68833f57e712681a88bf5d
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-04-14 02:13:42 +0400

    Add update & delete functionalities

--- ProductCatalogWPF/MainWindow.xaml.cs
+++ ProductCatalogWPF/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ProductCatalogWPF
 {
@@ -21,6 +13,8 @@ namespace ProductCatalogWPF
     public partial class MainWindow : Window
     {
         public List<Product> Products { get; set; }
+
+        private Product _selectedProduct;
         public MainWindow()
         {
             this.DataContext = this;
@@ -30,47 +24,81 @@ namespace ProductCatalogWPF
                 new Product()
                 {
                     Name = "Cola",
-                    Description = "Lab lab",
-                    Price = 10,
-                    Quantity = 100,
+                    Price = 2,
+                    Quantity = 120,
                 },
                 new Product()
                 {
                     Name = "Pepsi",
-                    Description = "Lab lab",
-                    Price = 10,
-                    Quantity = 100,
+                    Price = 5,
+                    Quantity = 200,
                 },
                 new Product()
                 {
                     Name = "Cips",
-                    Description = "Lab lab",
-                    Price = 10,
-                    Quantity = 100,
+                    Price = 1,
+                    Quantity = 50,
                 },
                 new Product()
                 {
                     Name = "Bizon",
-                    Description = "Lab lab",
-                    Price = 10,
-                    Quantity = 100,
+                    Price = 1.2,
+                    Quantity = 400,
                 },
                 new Product()
                 {
                     Name = "Hell",
-                    Description = "Lab lab",
-                    Price = 10,
-                    Quantity = 100,
+                    Price = 2,
+                    Quantity = 600,
                 },
                 new Product()
                 {
                     Name = "Beer",
-                    Description = "Lab lab",
-                    Price = 10,
-                    Quantity = 100,
+                    Price = 5,
+                    Quantity = 350,
                 },
             };
             InitializeComponent();
+        }
+
+        private void ButtonAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(ListBoxProducts.SelectedItem is Product item))
+                return;
+
+            _selectedProduct = item;
+
+            ImageProduct.Source = String.IsNullOrWhiteSpace(item.ImagePath) ? null: new BitmapImage(new Uri(item.ImagePath));
+            TextBoxProductName.Text = item.Name;
+            TextBoxProductPrice.Text = item.Price.ToString("F");
+            TextBoxProductQuantity.Text = item.Quantity.ToString();
+        }
+
+        private void ButtonUpdate_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_selectedProduct == null)
+                return;
+
+            _selectedProduct.Name = TextBoxProductName.Text;
+            _selectedProduct.Price= Convert.ToDouble(TextBoxProductPrice.Text);
+            _selectedProduct.Quantity = Convert.ToInt32(TextBoxProductQuantity.Text);
+
+            RefreshListBox();
+        }
+
+        private void RefreshListBox()
+        {
+            ListBoxProducts.ItemsSource = null;
+            ListBoxProducts.ItemsSource = Products;
+        }
+
+        private void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_selectedProduct == null)
+                return;
+
+            Products.Remove(_selectedProduct);
+            RefreshListBox();
         }
     }
 }
--- ProductCatalogWPF/Product.cs
+++ ProductCatalogWPF/Product.cs
@@ -2,8 +2,8 @@
 {
     public class Product
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
         public string ImagePath { get; set; }
