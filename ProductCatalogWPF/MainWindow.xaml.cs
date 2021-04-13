47d16bf42fd37aeb9b32a190d7af7407db09d02e
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-04-13 18:07:09 +0400

    Add Product Catalog

---
+++ ProductCatalogWPF/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductCatalogWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> Products { get; set; }
+        public MainWindow()
+        {
+            this.DataContext = this;
+
+            Products = new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Cola",
+                    Description = "Lab lab",
+                    Price = 10,
+                    Quantity = 100,
+                },
+                new Product()
+                {
+                    Name = "Pepsi",
+                    Description = "Lab lab",
+                    Price = 10,
+                    Quantity = 100,
+                },
+                new Product()
+                {
+                    Name = "Cips",
+                    Description = "Lab lab",
+                    Price = 10,
+                    Quantity = 100,
+                },
+                new Product()
+                {
+                    Name = "Bizon",
+                    Description = "Lab lab",
+                    Price = 10,
+                    Quantity = 100,
+                },
+                new Product()
+                {
+                    Name = "Hell",
+                    Description = "Lab lab",
+                    Price = 10,
+                    Quantity = 100,
+                },
+                new Product()
+                {
+                    Name = "Beer",
+                    Description = "Lab lab",
+                    Price = 10,
+                    Quantity = 100,
+                },
+            };
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProductCatalogWPF/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductCatalogWPF
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
